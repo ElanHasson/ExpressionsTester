f67c200e8606a9d0dc2696c2a7ec7fb1ec6649be
Elan Hasson <elan@elanhasson.com>
2018-12-18 09:44:35 -0500

    Combining constant values and method calls into a single parameterless lambda. (WIP)

--- ExpressionsTester/Program.cs
+++ ExpressionsTester/Program.cs
@@ -7,30 +7,51 @@ namespace ExpressionsTester
     {
         private static void Main(string[] args)
         {
-            var concatter = ConcatStringsExpression().Compile()(new object[] {"hello", " world"});
-            Console.WriteLine(concatter);
+            var stringConcat = ConcatStringsExpression();
+            Console.WriteLine($"ConcatStrings: {stringConcat.Compile()(new object[] { "hello", " world" })}");
+
+
+            // Passing a string
+            var executeExpressionToUpper = BuildStaticMethodCallExpression<string>(typeof(ToUpper), nameof(ToUpper.Execute));
+            Console.WriteLine($"{nameof(BuildStaticMethodCallExpression)}<string>(typeof(ToUpper), nameof(ToUpper.Execute)): {executeExpressionToUpper.Compile()("hello")}");
+
+            ParameterExpression upperInputParam =
+                Expression.Parameter(typeof(string), "p0");
+             
+            var expressionBlock = Expression.Block(Expression.Assign(upperInputParam, Expression.Constant("hello", typeof(string))), executeExpressionToUpper);
+            var lambda = Expression.Lambda<Func<string>>(expressionBlock);
+
+            Console.WriteLine(lambda.Compile()());
         }
 
-        public static ConstantExpression BuildConstant<T>(T constant)
+
+
+        /// <summary>
+        /// Builds an expression for a given type and method taking a single string parameter.
+        /// </summary>
+        /// <returns>An expression.</returns>
+        public static Expression<Func<string, TReturnType>> BuildStaticMethodCallExpression<TReturnType>(Type type, string methodName)
         {
-            return Expression.Constant(constant, typeof(T));
+            var parameterParam = Expression.Parameter(typeof(string), "p0");
+
+            var methodInfo = type.GetMethod(methodName, new[] { typeof(string) });
+            var result = Expression.Call(methodInfo, parameterParam);
+
+            return Expression.Lambda<Func<string, TReturnType>>(result, parameterParam);
         }
 
-
+        /// <summary>
+        /// Builds an expression for <see cref="String.Concat(object[])"/>
+        /// </summary>
+        /// <returns>An expression.</returns>
         public static Expression<Func<object[], string>> ConcatStringsExpression()
         {
             var parameterParam = Expression.Parameter(typeof(object[]), "objectsToConcat");
 
-            // String.Concat(object[])
             var stringConcat = typeof(string).GetMethod(nameof(string.Concat), new[] {typeof(object[])});
             var result = Expression.Call(stringConcat, parameterParam);
 
             return Expression.Lambda<Func<object[], string>>(result, parameterParam);
-        }
-
-        public static Expression BuildLambda(BlockExpression blockExpression)
-        {
-            return Expression.Lambda(blockExpression);
         }
     }
 }
---
+++ ExpressionsTester/ToUpper.cs
@@ -0,0 +1,18 @@
+﻿namespace ExpressionsTester
+{
+    /// <summary>
+    /// A class wrapper for <see cref="string.ToUpper()"/>
+    /// </summary>
+    public class ToUpper
+    {
+        /// <summary>
+        /// Wraps <see cref="string.ToUpper()"/>.
+        /// </summary>
+        /// <param name="input">The string to uppercase</param>
+        /// <returns>The uppercase string</returns>
+        public static string Execute(string input)
+        {
+            return input.ToUpper();
+        }
+    }
+}
