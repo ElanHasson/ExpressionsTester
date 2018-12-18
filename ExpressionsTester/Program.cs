eac5bf4eb73f5f088a7ef0724bbb9016cc79f322
Elan Hasson <elan@elanhasson.com>
2018-12-18 02:34:44 -0500

    Add StringConcatter
    (cherry picked from commit ad9c81b40b8fe3c1c74eb88561fe1e379b07d5ba)

--- ExpressionsTester/Program.cs
+++ ExpressionsTester/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace ExpressionsTester
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var concatter = ConcatStringsExpression().Compile()(new object[] {"hello", " world"});
+            Console.WriteLine(concatter);
+        }
+
+        public static ConstantExpression BuildConstant<T>(T constant)
+        {
+            return Expression.Constant(constant, typeof(T));
+        }
+
+
+        public static Expression<Func<object[], string>> ConcatStringsExpression()
+        {
+            var parameterParam = Expression.Parameter(typeof(object[]), "objectsToConcat");
+
+            // String.Concat(object[])
+            var stringConcat = typeof(string).GetMethod(nameof(string.Concat), new[] {typeof(object[])});
+            var result = Expression.Call(stringConcat, parameterParam);
+
+            return Expression.Lambda<Func<object[], string>>(result, parameterParam);
+        }
+
+        public static Expression BuildLambda(BlockExpression blockExpression)
+        {
+            return Expression.Lambda(blockExpression);
         }
     }
 }
